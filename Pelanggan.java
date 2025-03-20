f8863788b3d992833cca8731c8be6397a4a275f8
wirapatigilang <wirapatigilang@gmail.com>
2025-03-20 20:20:27 +0700

    Add Pelanggan.java

---
+++ Pelanggan.java
@@ -0,0 +1,24 @@
+public class Pelanggan {
+    private String nama;
+    private String alamat;
+    private String nomorTelpon;
+    private String email;
+
+    public Pelanggan () {
+        this.nama = "";
+        this.alamat = "";
+        this.nomorTelpon = "";
+        this.email = "";
+    }
+
+ 
+    public Pelanggan (String nama, String alamat, String nomorTelpon, String email) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.nomorTelpon = nomorTelpon;
+        this.email = email;
+    }
+
+    
+
+}
