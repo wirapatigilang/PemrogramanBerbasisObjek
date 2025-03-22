94db81633253a48ae887aa5c317ece856dabd61f
wirapatigilang <wirapatigilang@gmail.com>
2025-03-22 22:45:51 +0700

    Perbaikan pada main

--- Main.java
+++ Main.java
@@ -1,27 +1,33 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // Membuat Tipe Kamar
-        TipeKamar Superior = new TipeKamar("Superior", 200000, List.of("TV", "Water Heater", "Double Bed"));
-        TipeKamar Suite = new TipeKamar("Suite", 300000, List.of("TV", "Water Heater", "King Size Bed", "Balkon"));
+        TipeKamar superior = new TipeKamar("Superior", 200000, new ArrayList<>(List.of("TV", "Water Heater", "Double Bed")));
+        TipeKamar suite = new TipeKamar("Suite", 300000, new ArrayList<>(List.of("TV", "Water Heater", "King Size Bed", "Balkon")));
 
         // Membuat Kamar
-        Kamar Kamar001 = new Kamar("001", "Tersedia", Superior);
-        Kamar Kamar002 = new Kamar("002", "Tersedia", Superior);
-        Kamar Kamar003 = new Kamar("003", "Tersedia", Superior);
-        Kamar Kamar004 = new Kamar("004", "Tersedia", Suite);
-        Kamar Kamar005 = new Kamar("005", "Tersedia", Suite);
+        Kamar kamar001 = new Kamar("001", "Tersedia", superior);
+        Kamar kamar002 = new Kamar("002", "Tersedia", superior);
+        Kamar kamar003 = new Kamar("003", "Tersedia", superior);
+        Kamar kamar004 = new Kamar("004", "Tersedia", suite);
+        Kamar kamar005 = new Kamar("005", "Tersedia", suite);
 
-        // Membuat Pelanggan
-        Pelanggan Pelanggan1 = new Pelanggan("Gilang", "Lombok", "082340006777", "wirapatigilang@gmail.com", null);
-        Pelanggan1.tambahPesanan(new PesananKamar(null, "0001", LocalDate.of(2025, 10, 3), LocalDate.of(2025, 10, 5), "Reservasi", List.of(Kamar001)));
-        //Pelanggan1.infoPelanggan();
+        // Membuat Pelanggan dan Pesanan Kamar
+        Pelanggan pelanggan1 = new Pelanggan("Gilang", "Lombok", "082340006777", "wirapatigilang@gmail.com", null);
+        PesananKamar pesanan1 = new PesananKamar(pelanggan1, "P001", LocalDate.now(), LocalDate.now().plusDays(3), "Ongoing", new ArrayList<>());
+        pelanggan1.setDaftarPesanan(pesanan1);
+        pelanggan1.tambahPesanan(kamar005);
+        pelanggan1.tambahPesanan(kamar001);
+
+        // Menampilkan info pelanggan
+        pelanggan1.infoPelanggan();
 
         // Membuat Ulasan
-        Ulasan ulasan1 = new Ulasan("U001", Pelanggan1, Kamar005, 5, "Kamarnya ganteng", LocalDate.now());
-        //ulasan1.printUlasan();
+        // Ulasan ulasan1 = new Ulasan("U001", pelanggan1, kamar005, 5, "Kamarnya ganteng", LocalDate.now());
+        // ulasan1.printUlasan();
 
         // Membuat Pembayaran
         Pembayaran pembayaran1 = new Pembayaran("Kartu Kredit", 500000, "Belum Lunas", LocalDate.now());
